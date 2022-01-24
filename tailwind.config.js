edb109555e0d1e39f63dc78cb1e9603cfe93e3f9
hiro3730 <minami12199@i.softbank.jp>
2022-01-24 15:05:05 +0900

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const colors=require("tailwindcss/colors")
+
 module.exports = {
   mode:"jit",
   //pargeと同じ？
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     
@@ -18,15 +20,15 @@ module.exports = {
        current: "currentColor",
        black: colors.black,
        white: colors.white,
-       "text-black": '#243c5a',
-       "text-green": "#008c8d",
-       "bg-green": "#6bc2c3",
-       "bg-black": "#262c3a",
-       "progress-pale": "#c5eaea",
-       "progress-deep": "#2bb9ba",
-       "border-green": "#c5eaea",
+       "text-black": '#384359',
+       "text-green":'#008c8d' ,
+       "bg-green":'#6bc2c3' ,
+       "bg-black":'#262c3a' ,
+       "progress-pale":'#c5eaea' ,
+       "progress-deep":'#2bb9ba' ,
+       "border-green":'#c5eaea' ,
       }, 
     },
   },
   plugins: [],
-}
+};
