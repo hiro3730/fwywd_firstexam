4208d791d701bb02cb44933da882adefbd9d2c63
hiro3730 <minami12199@i.softbank.jp>
2022-01-24 14:30:43 +0900

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
 module.exports = {
+  mode:"jit",
+  //pargeと同じ？
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    
+    //中央揃え
+    container:{
+      center:true,
+    },
+    extend: {
+      //カラーコード
+     colors: {
+       transparent: "transparent",
+       current: "currentColor",
+       black: colors.black,
+       white: colors.white,
+       "text-black": '#243c5a',
+       "text-green": "#008c8d",
+       "bg-green": "#6bc2c3",
+       "bg-black": "#262c3a",
+       "progress-pale": "#c5eaea",
+       "progress-deep": "#2bb9ba",
+       "border-green": "#c5eaea",
+      }, 
+    },
   },
   plugins: [],
 }
